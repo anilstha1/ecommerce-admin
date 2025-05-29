d4934f775a9d975bbbe70f384ba6be8e1964d692
anilstha1 <anilandshrestha1@gmail.com>
2025-05-29 13:07:21 +0545

    fix deployment issue

--- src/app/api/billboards/[billboardId]/route.ts
+++ src/app/api/billboards/[billboardId]/route.ts
@@ -1,8 +1,8 @@
-import {NextRequest, NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 import prisma from "@/lib/db";
 
 export async function GET(
-  req: NextRequest,
+  req: Request,
   {params}: {params: {billboardId: string}}
 ) {
   try {
