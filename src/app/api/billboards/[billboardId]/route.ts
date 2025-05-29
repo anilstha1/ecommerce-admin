e8b343cde94d5192bbddf0afd3b90f1253fc392e
anilstha1 <anilandshrestha1@gmail.com>
2025-05-29 12:35:45 +0545

    small changes

--- src/app/api/billboards/[billboardId]/route.ts
+++ src/app/api/billboards/[billboardId]/route.ts
@@ -1,10 +1,7 @@
-import {NextRequest, NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 import {prisma} from "@/lib/db";
 
-export async function GET(
-  req: NextRequest,
-  {params}: {params: {billboardId?: string}}
-) {
+export async function GET({params}: {params: {billboardId: string}}) {
   try {
     const {billboardId} = params;
 
